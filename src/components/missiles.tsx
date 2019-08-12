79fc8870b27bbeea989d4937b72c0a5dcf21dc2c
= <=>
2019-08-12 09:02:54 +0530

    missile positioning

--- src/components/missiles.tsx
+++ src/components/missiles.tsx
@@ -7,28 +7,29 @@ const Missile = styled.div`
 	width: 30px;
 	fill: #aaa;
 	transform: rotate(40deg);
+	position: absolute;
+	bottom: 10px
 `;
 
 const MissilesContainer = styled.div`
 	flex-grow: 1;
-	position: relative;
-	
+	position: relative;	
 `;
 
 const Bounce2s = styled(Missile)`
   	animation: 3s ${keyframes`${bounceInDown}`};
 `;
-const Bounce3s = styled(Missile)`
-  	animation: 4s ${keyframes`${bounceInDown}`};
-`;
-const Bounce4s = styled(Missile)`
-  	animation: 5s ${keyframes`${bounceInDown}`};
-`;
+// const Bounce3s = styled(Missile)`
+//   	animation: 4s ${keyframes`${bounceInDown}`};
+// `;
+// const Bounce4s = styled(Missile)`
+//   	animation: 5s ${keyframes`${bounceInDown}`};
+// `;
 
 const Missiles: React.FC = () => {
   return (
     <MissilesContainer>
-		<Bounce2s style={}>
+		<Bounce2s style={{ left: '400px' }}>
 			<ReactSVG src="missile.svg" />
 		</Bounce2s>
     </MissilesContainer>
